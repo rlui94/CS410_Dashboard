af3012344eaf668566f2063c5b0e6949a4194bc2
Ryan Lui <ryanlui.college@gmail.com>
2020-07-21 00:09:44 -0700

    initial implementation of type chart

--- API/api.js
+++ API/api.js
@@ -42,3 +42,52 @@ async function getViaLoc(minLat=-90, minLong=-180, maxLat=90, maxLong=180, start
     console.log('Results', results);
 }
 
+function displayChart(usgsObj, chartNode){
+    let results = {}, labels = [], vals = [];
+    // count occurrences of each type
+    for (let i=0; i<usgsObj.features.length; ++i){
+        let type = usgsObj.features[i].properties.type;
+        if(!results[type]){
+            results[type] = 1;
+        }
+        else {
+            results[type] = results[type] + 1;
+        }
+    }
+    // get labels and values arrays chart requires
+    labels = Object.getOwnPropertyNames(results);
+    vals = Object.values(results);
+    let doughnut = new Chart(chartNode,{
+        type: 'doughnut',
+        data: {
+          datasets: [{
+            data: vals,
+            backgroundColor: [
+              'rgba(54, 162, 235, 0.8)',
+              'rgba(255, 206, 86, 0.8)',
+              'rgba(255, 99, 132, 0.8)',
+              'rgba(75, 192, 192, 0.8)',
+              'rgba(153, 102, 255, 0.8)',
+              'rgba(255, 159, 64, 0.8)',
+              'rgba(199, 199, 199, 0.8)',
+            ],
+            borderColor: [
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(159, 159, 159, 1)',
+            ]
+          }],
+          labels: labels,
+          },
+          options:{
+            legend: {
+              position: 'bottom'
+            },
+          },
+      })
+}
+
