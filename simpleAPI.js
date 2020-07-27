a6fa9b3538dd1db088817caae78966d3a21aa4ff
John <johlewis@pdx.edu>
2020-07-26 19:28:25 -0700

    new api function for day, week, month calls

---
+++ simpleAPI.js
@@ -0,0 +1,15 @@
+//URLs for monthly, weekly, and daily earthquake geoJSON info
+const url_month = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+const url_week = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+const url_day = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+//API access function
+async function apiInfo(url){
+    let response = await fetch(url);
+    if(response.status == 200){
+        let data = await response.json();
+        console.log(data);
+        return data;
+    }
+    throw new Error(response.status);
+}
