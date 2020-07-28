268b5554e0e891033ec0e717015444102fcd910b
Ryan Lui <ryanlui.college@gmail.com>
2020-07-28 11:11:22 -0700

    more plot formatting

--- JS/chart.js
+++ JS/chart.js
@@ -138,37 +138,35 @@ function makeScatterChart(usgsObj, chartNode){
       type: 'scatter',
       data: {
         datasets: [{
+          label: 'Refresh Chart',
           data: results,
-          backgroundColor: [
-            'rgba(54, 162, 235, 0.8)',
-            'rgba(255, 206, 86, 0.8)',
-            'rgba(255, 99, 132, 0.8)',
-            'rgba(75, 192, 192, 0.8)',
-            'rgba(153, 102, 255, 0.8)',
-            'rgba(255, 159, 64, 0.8)',
-            'rgba(199, 199, 199, 0.8)',
-          ],
-          borderColor: [
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(255, 99, 132, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(159, 159, 159, 1)',
-          ]
+          pointBackgroundColor: 'teal',
         }],
         labels: 'Scatter Dataset',
         },
         options:{
           scales: {
             xAxes: [{
+              scaleLabel:{
+                display: true,
+                labelString: 'Time',
+              },
               type: 'time',
               distribution: 'linear',
               time:{
-                min: results[results.length-1].x,
+                min: moment().subtract(1, 'hour'),
+                max: moment(),
               },
               bounds: 'data',
+            }],
+            yAxes: [{
+              scaleLabel:{
+                display: true,
+                labelString: 'Magnitude',
+              },
+              ticks: {
+                min: 0,
+              }
             }]
           }
         },
@@ -196,7 +194,6 @@ async function createChartViaForm(formID, chartID){
 async function createRefreshChart(chartNode){
   apiInfo(refreshUrl)
   .then(data => {
-    console.log(data);
     makeScatterChart(data, chartNode);
   })
   .catch(reason => console.log(reason.message));
