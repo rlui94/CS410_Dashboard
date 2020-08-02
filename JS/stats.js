e7dd83a11d466edf15c2a3d25af2a44f91805375
John <johlewis@pdx.edu>
2020-08-01 17:10:03 -0700

    adjusted resizing with stats tiles added

--- JS/stats.js
+++ JS/stats.js
@@ -4,20 +4,16 @@ var stats = document.getElementById("quickStats");
 apiInfo(url_day)
     .then(data => {
         var total = document.getElementById("total");
-        total.innerHTML = "<h5>Number of Quakes</h5><br><h1>"+data.features.length+"</h1>";
-        stats.appendChild(total);
+        total.innerHTML = "<p>Total Quakes</p><h2>"+data.features.length+"</h2>";
 
         var largest = document.getElementById("largest");
-        largest.innerHTML = "<h5>Largest Magnitude</h5><br><h1>" + findMax(data.features).mag +"</h1><h6><i>click tile to view location</i></h6>";
-        stats.appendChild(largest);
+        largest.innerHTML = "<p>Largest Quake</p><h2>" + findMax(data.features).mag +"</h2>";
     })
     .catch(reason => console.log(reason.message));
 
 apiInfo(url_day_sig)
     .then(data => {
         var sig = document.getElementById("significant");
-        sig.innerHTML = "<h5>Significant Quakes</h5><br><h1>" + data.features.length + "</h1><h6><i>click tile to view locations on map</i></h6>";
-        stats.appendChild(sig);
-
+        sig.innerHTML = "<p>Caused Damage</p><h2>" + data.features.length + "</h2>";
     })
     .catch(reason => console.log(reason.message));
