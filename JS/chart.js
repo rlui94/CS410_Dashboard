8c85167d8adf2bcac7a81966ac8304e914b67ae6
Ryan Lui <ryanlui.college@gmail.com>
2020-08-05 01:22:06 -0700

    add more charts

--- JS/chart.js
+++ JS/chart.js
@@ -2,6 +2,10 @@
 var refreshChart = null;
 var refreshUrl = url_hour;
 var interval = 60000;
+// for modifying bar chart
+var barChart = null;
+var barUrl = url_week;
 
 window.onload = createRefreshChart('refresh-chart');
 let refresh = window.setInterval(function(){updateRefreshChart()}, interval);
+window.onload = createQuakesChart('bar-chart');
