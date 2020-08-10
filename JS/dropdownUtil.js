37170063ef0eaf94dac214ca027eeca66ebd651d
John <johlewis@pdx.edu>
2020-08-09 17:16:30 -0700

    scroll to map on "Near Me" dropdown

--- JS/dropdownUtil.js
+++ JS/dropdownUtil.js
@@ -59,6 +59,7 @@ function changeToMonth() {
 //creates USGS api query based on user location
 //calls showStats() and displayWeek()
 function changeToMe() {
+    document.getElementById("mapContainer").scrollIntoView(false);
     map.locate();
     map.on("locationerror", function(e){
         alert(e.message);
